50fffd30c347777df6cdae6a34b716e4fed79889
Harun Ozceyhan <harunozceyhan@gmail.com>
2019-12-18 10:30:32 +0300

    initial commit

---
+++ src/main/java/com/smart/config/CustomHibernateInterceptor.java
@@ -0,0 +1,64 @@
+package com.smart.config;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import static net.logstash.logback.argument.StructuredArguments.kv;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smart.model.base.BaseEntity;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+import java.io.Serializable;
+import java.util.Iterator;
+
+@Component
+public class CustomHibernateInterceptor extends EmptyInterceptor {
+
+    private String crudtype = "";
+    private static final long serialVersionUID = -1;
+    private static final Logger LOGGER = LogManager.getLogger("jsonLogger");
+
+    @Override
+    public String onPrepareStatement(String sql) {
+        crudtype = "";
+        if (sql.startsWith("update")) {
+            crudtype = "UPDATE";
+        } else if (sql.startsWith("insert")) {
+            crudtype = "INSERT";
+        }
+        return super.onPrepareStatement(sql);
+    }
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public void postFlush(Iterator entities) {
+        super.postFlush(entities);
+        while (entities.hasNext()) {
+            Object entityObj = entities.next();
+            if (crudtype.equals("INSERT") || crudtype.equals("UPDATE")) {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                LOGGER.info(asJsonString(entityObj), kv("app", "Sbo-Service"), kv("type", crudtype), kv("className", entityObj.getClass().getSimpleName()), kv("classPackageName", entityObj.getClass().getName()), kv("id", BaseEntity.class.cast(entityObj).getId().toString()), kv("user", authentication == null ? "" : authentication.getName()));
+            }
+        }
+    }
+
+    @Override
+	public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LOGGER.info(asJsonString(entity), kv("app", "Sbo-Service"), kv("type", "DELETE"), kv("className", entity.getClass().getSimpleName()), kv("classPackageName", entity.getClass().getName()), kv("id", id), kv("user", authentication == null ? "" : authentication.getName()));
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/com/smart/config/RestConfiguration.java
@@ -0,0 +1,40 @@
+package com.smart.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+    }
+
+    @Bean
+    @Primary
+    Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        Validator validator = validator();
+        validatingListener.addValidator("beforeCreate", validator);
+        validatingListener.addValidator("beforeSave", validator);
+    }
+}
