5506702971f3aca52363864c77e42747bd251418
Harun Özceyhan <harunozceyhan@smartiys.com>
2019-12-30 22:48:52 +0300

    fix: cors enabled

--- src/main/java/com/smart/config/JwtTokenAuthenticationFilter.java
+++ src/main/java/com/smart/config/JwtTokenAuthenticationFilter.java
@@ -19,59 +19,71 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-public class JwtTokenAuthenticationFilter extends  OncePerRequestFilter {
-    
-	private String appName;
-	private String signingKey;
-	private String contextPath;
+public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
-	public JwtTokenAuthenticationFilter(String appName, String signingKey, String contextPath) {
-		this.signingKey = signingKey;
-		this.appName = appName;
-		this.contextPath = contextPath;
-	}
+    private String appName;
+    private String signingKey;
+    private String contextPath;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-		try {
-			UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
-			SecurityContextHolder.getContext().setAuthentication(authentication);
-		} catch (Exception e) {
-			SecurityContextHolder.clearContext();
-		}
-		chain.doFilter(request, response);
-	}
+    public JwtTokenAuthenticationFilter(String appName, String signingKey, String contextPath) {
+        this.signingKey = signingKey;
+        this.appName = appName;
+        this.contextPath = contextPath;
+    }
 
-	@SuppressWarnings("unchecked")
-	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        try {
+            UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception e) {
+            SecurityContextHolder.clearContext();
+        }
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader("Access-Control-Allow-Headers",
+                "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.addHeader("Access-Control-Expose-Headers",
+                "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        chain.doFilter(request, response);
+    }
+
+    @SuppressWarnings("unchecked")
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
         if (token != null) {
-			try {	
-				KeyFactory kf = KeyFactory.getInstance("RSA");
-				X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.decodeBase64(signingKey));
-				RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
-				DecodedJWT decodedJWT = JWT.require(Algorithm.RSA256(pubKey, null)).build().verify(token.replace("Bearer ", ""));
-				String user = decodedJWT.getClaim("preferred_username").asString();
-				HashMap<String, ArrayList<String>> clientMap = ((HashMap<String, ArrayList<String>>)decodedJWT.getClaim("resource_access").asMap().get(appName));
-				if (clientMap == null) {
-					throw new AccessDeniedException("Client Service Not Found!");
-				} else {
-					String defaultMapping = request.getRequestURI().replace(contextPath + "/","").split("/")[0];
-					Long permissionCount = clientMap.get("roles").stream().filter(permission -> permission.equals("*") || permission.equals(defaultMapping + ":*") || permission.equals(defaultMapping + ":" + request.getMethod().toLowerCase())).count();
-					if (permissionCount == 0) {
-						throw new AccessDeniedException("Permission Not Found!");
-					}
-				}
-				if (user != null) {
-					return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
-				}
-				return null;
-			} catch (Exception e) {
-				request.setAttribute("exception", e);
-				SecurityContextHolder.clearContext();
-			}
-		}
-		SecurityContextHolder.clearContext();
+            try {
+                KeyFactory kf = KeyFactory.getInstance("RSA");
+                X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.decodeBase64(signingKey));
+                RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
+                DecodedJWT decodedJWT = JWT.require(Algorithm.RSA256(pubKey, null)).build()
+                        .verify(token.replace("Bearer ", ""));
+                String user = decodedJWT.getClaim("preferred_username").asString();
+                HashMap<String, ArrayList<String>> clientMap = ((HashMap<String, ArrayList<String>>) decodedJWT
+                        .getClaim("resource_access").asMap().get(appName));
+                if (clientMap == null) {
+                    throw new AccessDeniedException("Client Service Not Found!");
+                } else {
+                    String defaultMapping = request.getRequestURI().replace(contextPath + "/", "").split("/")[0];
+                    Long permissionCount = clientMap.get("roles").stream()
+                            .filter(permission -> permission.equals("*") || permission.equals(defaultMapping + ":*")
+                                    || permission.equals(defaultMapping + ":" + request.getMethod().toLowerCase()))
+                            .count();
+                    if (permissionCount == 0) {
+                        throw new AccessDeniedException("Permission Not Found!");
+                    }
+                }
+                if (user != null) {
+                    return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+                }
+                return null;
+            } catch (Exception e) {
+                request.setAttribute("exception", e);
+                SecurityContextHolder.clearContext();
+            }
+        }
+        SecurityContextHolder.clearContext();
         return null;
     }
 
